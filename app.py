9d1059b1c7e17c5fe05897fcd3b02f1b79af6e16
Gift_M <mofokeng_mpho@icloud.com>
2024-12-07 23:47:20 +0200

    Added some functions and added html for home page

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/deposit")
+def deposit():
+    return render_template("deposit.html")
+
+@app.route("/update")
+def update():
+    return render_template("UpdateAccount.html")
+
+@app.route("/withdraw")
+def withdraw():
+    return render_template("withdraw.html")
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
