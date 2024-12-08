a4176fbe0f29cf54e1be802426336e90a6c179c0
Gift_M <mofokeng_mpho@icloud.com>
2024-12-08 11:39:34 +0200

    Added functionality for login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect,session
 from database import *
 
 app = Flask(__name__)
@@ -6,14 +6,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banking_system.db'
 
 db.init_app(app)
+app.secret_key = "bngngjewdfwij@#$%^&*"
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/login")
+@app.route("/login",methods = ["GET","POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if username in user and user[username] == password:
+            session['user'] = username
+            print("Login Successful.")
+            return render_template("homepage.html")
+        
+    else:
+        print("Username does not exist")
+        return render_template("login.html")
 
 @app.route("/register",methods = ["GET","POST"])
 def register():
